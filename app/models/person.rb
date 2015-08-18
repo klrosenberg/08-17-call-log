e7921edeb517ef147525f701927975c2679bc5d9
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-08-18 09:46:50 -0500

    List all persons with numbers in ASC order

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,14 @@
 class Person < ActiveRecord::Base
+  
+  def self.set_client
+    Twilio::REST::Client.new ENV['account_sid'], ENV['auth_token']
+  end
+  
+  def self.list
+    Person.all.order('last_call ASC')
+  end
+  
+  def self.least_recent_contact
+    Person.where.not(last_attempt: '2015-08-17').order("last_call ASC").first
+  end
 end
